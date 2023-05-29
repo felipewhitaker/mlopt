56f0f8a23f5c0b7915a261235e5a2fdd987c85f8
Felipe <felipewhitaker+frog@gmail.com>
2023-05-29 16:24:05 -0300

    Fix Holistic commit

--- exercises/regression/holistic/holistic.jl
+++ exercises/regression/holistic/holistic.jl
@@ -6,44 +6,22 @@ using LinearAlgebra
 n_data = 100
 n_feat, n_corr = 3, 2 # number of (features, fake features)
 total_feat = n_feat + n_corr
-m = 2 # number of variables to select
+m = n_corr # number of variables to select
 
 Xt = rand(Normal(0, 1), n_data, n_feat)
-# coef = [1 1 0]' # rand(Uniform(0, 1), size(Xt, 2), n_corr)
-Xf = hcat(Xt * [1 0 0]', Xt * [0 1 0]') .+ rand(Normal(0, 0.04), n_data, 2)
+coef = rand(Uniform(0, 1), size(Xt, 2), n_corr)
+Xf = Xt * coef .+ rand(Normal(0, 0.04), n_data, 2)
 X = hcat(Xt, Xf)
 
-# B = vcat(rand(Uniform(0, 1), n_feat), zeros(n_corr))
-# y = X * B + rand(Normal(0, 1), size(Xt, 1))
 ev = eigvals(X'X)
 V = eigvecs(X'*X)[:, sortperm(abs.(ev))][:, 1:m] # size = (p, m) = (total_feat, m)
 
-M = 1e3
+M = 1e1
 p = total_feat
-delta = 1e-6
+delta = 1e-1
 
 model = Model(HiGHS.Optimizer)
-function scalar_problem(model)
-    @variable(model, z[1:p], Bin)
-    @variable(model, a[1:p])
-    @variable(model, theta[1:m])
-
-    @constraint(model, [j in 1:p], a[j] == sum(theta[i] * V[j, i] for i in 1:m))
-
-    @constraint(model, sum(theta[i] for i in 1:m) >= delta)
-    @constraint(model, sum(theta[i] for i in 1:m) <= - delta)
-
-    @constraint(model, [j in 1:p], a[j] <= M * z[j])
-    @constraint(model, [j in 1:p], a[j] >= -M * z[j])
-
-    @objective(model, Min, sum(z))
-    optimize!(model)
-    value.(z)
-end
-scalar_problem(model)
-
-
-model = Model(HiGHS.Optimizer)
+set_silent(model)
 function problem(model)
 
     global p, m
@@ -52,23 +30,38 @@ function problem(model)
     @variable(model, theta[1:m])
     @variable(model, z[1:p], Bin)
     @variable(model, a[1:p])
+    @variable(model, y)
 
     @constraint(model, a .== V * theta)
-    @constraint(model, sum(theta) >= delta)
-    @constraint(model, sum(theta) <= -delta)
+    # following given tip
+    @constraint(model, y >= delta)
+    @constraint(model, y >= sum(theta))
+    @constraint(model, y <= -sum(theta))
+    # big M strategy for absolute value constraints
     @constraint(model, a .>= - M .* z)
     @constraint(model, a .<= M .* z)
 
-    # @constraint(model, theta .>= delta)
-    # @constraint(model, theta .<= -delta)
-
     @objective(model, Min, sum(z))
-    optimize!(model)
-
-    if JuMP.termination_status(model) == JuMP.OPTIMAL
-        return (ob = objective_value(model), a = value.(a), z = value.(z), theta = value.(theta))
-    end
-    return (ob = nothing, a = nothing, z = nothing, theta = nothing)
 end
 
-ob, a, z, theta = problem(model)
+problem(model)
+
+for i in 1:2
+    println("Iteration $i")
+    optimize!(model)
+
+    if termination_status(model) == MOI.INFEASIBLE
+        println("Infeasible")
+        break
+    end
+
+    z = value.(model[:z])
+    println("z = $z")
+    
+    not_zero = (z .!= 0)
+    @constraint(model, z' * not_zero <= sum(not_zero) - 1)
+end
+
+# although there are two correlated features in the data, the algorithm 
+# was not able to identify that the last two features should have been
+# zero; reaching infeability on the second iteration. 
