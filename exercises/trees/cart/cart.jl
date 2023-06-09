9a3c03d159338304e5617484553b9655da0ce73d
Felipe Whitaker <felipewhitaker@gmail.com>
2023-06-09 20:35:06 -0300

    Fix `cart`

--- exercises/trees/cart/cart.jl
+++ exercises/trees/cart/cart.jl
@@ -1,5 +1,6 @@
 using DecisionTree: load_data, DecisionTreeClassifier
 using MLDataUtils: shuffleobs, splitobs
+using MLBase: mode as mlmode
 
 using JuMP
 using GLPK
@@ -35,67 +36,80 @@ possible_labels = unique(labels)
 int_labels = indexin(labels, possible_labels)
 
 # spliting train, val
-trainidx, validx = splitobs(shuffleobs(1:size(features)[1]), 0.7)
+trainperc = .4 # FIXME low computing resources
+trainidx, validx = splitobs(shuffleobs(1:size(features)[1]), trainperc)
 X_train, y_train = scaled_features[trainidx, :], int_labels[trainidx]
+# FIXME how to use the optimization model with the validation set?
 X_val, y_val = scaled_features[validx, :], int_labels[validx]
-
-X = X_train
-y = y_train
-depth = 2
 
 function oct(
     (X, y)::Tuple{AbstractMatrix, AbstractArray},
-    possible_labels::Integer,
+    possible_labels::Vector,
     depth::Integer,
     Nmin::Integer;
-    alpha::Float64 = 0.0
+    alpha::Float64 = 1e-1
 )
 
-	model = Model(GLPK.Optimizer)
-
+    # Define indexes
     p = 1:size(X)[end]
-	n = size(X)[1]
+    n = size(X)[1]
     K = 1:length(possible_labels)
     T = 2^(depth + 1) - 1
 
-	Tl = 1:(div(T, 2))
-    Tb = (div(T, 2) + 1):T
+    Tb = 1:(div(T, 2))
+    Tl = (div(T, 2)+1):T
+
+    # Define constants
+    Lhat = sum(y .== mlmode(y))
+
+    X_sort = sort(X, dims = 1)
+    epsilon = [
+        minimum([
+            X_sort[i + 1, j] - X_sort[i, j] 
+            for i = 1:(n-1) if X_sort[i + 1, j] != X_sort[i, j]
+        ])
+        for j in p
+    ]
+    epsilon_min = minimum(epsilon)
+    M1 = 1 + maximum(epsilon)
+    M2 = 1
+
+    # Define model
+    model = Model(GLPK.Optimizer)
 
 	@variable(model, a[m in p, t in Tb], Bin)
     @variable(model, b[t in Tb] >= 0)
     @variable(model, c[k in K, t in Tl])
 	@variable(model, d[t in Tb], Bin)
 
-    @variable(model, L[t in Tl])
+    @variable(model, L[t in Tl] >= 0)
     @variable(model, l[t in Tl])
 
     @variable(model, Nt[t in Tl])
     @variable(model, Nkt[k in K, t in Tl])
 
-    @variable(model, z[i in n, t in Tl])
+    @variable(model, z[i in 1:n, t in Tl])
 
 	@constraint(model, [t in Tl, k in K], L[t] >= Nt[t] - Nkt[k, t] - n * (1 - c[k, t]))
 	@constraint(model, [t in Tl, k in K], L[t] <= Nt[t] - Nkt[k, t] - n * c[k, t])
-	@constraint(model, L .>= 0)
 	@constraint(model, [t in Tl, k in K], Nkt[k, t] == 1 / 2 * sum((1 + y[i]) * z[i, t] for i in n))
 	@constraint(model, [t in Tl], Nt[t] == sum(z[:, t]))
-	@constraint(model, [t in Tl], sum(c[k, :]) == l[t])
+	@constraint(model, [t in Tl], sum(c[:, t]) == l[t])
 	@constraint(model, [t in Tb], sum(a[:, t]) == d[t])
 	@constraint(model, [t in Tb], b[t] <= d[t])
 	@constraint(model, [t in Tl], z[:, t] .<= l[t])
 	@constraint(model, [t in Tl], sum(z[:, t]) >= Nmin * l[t])
 	@constraint(model, [i in 1:n], sum(z[i, :]) == 1)
 
-    M1, M2 = 5, 5
 	for t in Tl
-        # b[Tb], but z[n, Tl]?
-		@constraint(model, [i in 1:n, m in anc_l(t)], a[m, :]' * X[i, :] <= b[t] + M1 * (1 - z[i, t]))
-		@constraint(model, [i in 1:n, m in anc_r(t)], a[m, t]' * X[i, :] >= b[t] - M2 * (1 - z[i, t]))
+        @constraint(model, [i in 1:n, j in anc_l(t)], a[:, j]'X[i, :] <= b[j] + M1 * (1 - z[i, t]) .- epsilon_min)
+		@constraint(model, [i in 1:n, j in anc_r(t)], a[:, j]'X[i, :] >= b[j] - M2 * (1 - z[i, t]))
 	end
 
-    Lhat = 1 # FIXME
-    @objective(model, Min, 1 / Lhat * sum(L) + alpha * sum(d))
-
+    @objective(model, Min, (1 / Lhat) * sum(L) + alpha * sum(d))
+    return model
 end
 
-oct((X_val, y_val), possible_labels, 3, 1)
+depth, Nmin = 2, 1
+model = oct((X_train, y_train), possible_labels, depth, Nmin)
+optimize!(model)
