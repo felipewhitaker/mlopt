b860a300ce50e36249602530f0bd7cdd98e30fa0
Felipe Whitaker <felipewhitaker+git@gmail.com>
2023-06-12 14:41:12 -0300

    Update `adaboost.jl` to include visualization

--- exercises/trees/adaboost/adaboost.jl
+++ exercises/trees/adaboost/adaboost.jl
@@ -4,7 +4,12 @@ using DecisionTree: build_stump, apply_tree, _weighted_error
 using DecisionTree: DecisionTreeClassifier, fit!, predict
 using IterTools: product
 
+using Plots
+
 using JuMP, Ipopt
+
+using Random: seed!
+seed!(42)
 
 function adaboost(
     (X_train, y_train), 
@@ -34,12 +39,20 @@ function adaboost(
         push!(estimators_weights, alpha)
 
         if i in metric_stops
-            pred_val = adaboost_predict(estimators, estimators_weights, X_val)
+            # # FIXME should train error consider the new 
+            # # estimators or shouldn't it?
+            # pred_train = adaboost_predict(
+            #     estimators[1:(end - 1)], 
+            #     estimators_weights[1:(end - 1)], 
+            #     X_val
+            # )
             err_train = sum(pred_train .!= y_train) / size(X_train, 1)
+
+            pred_val = adaboost_predict(estimators, estimators_weights, X_val)
             err_val = sum(pred_val .!= y_val) / size(X_val, 1)
             push!(train_errs, err_train)
             push!(val_errs, err_val)
-            push!(wdist, weights)
+            push!(wdist, deepcopy(weights))
         end
     end
     return estimators, estimators_weights, (train_errs, val_errs, wdist)
@@ -56,7 +69,7 @@ end
 
 features, labels = load_data("adult")
 
-subset = 1:10_000 # FIXME low computing resources
+subset = 1:10_000
 features = features[subset, [1, 3, 5, 11, 12, 13]] # keeping only numeric features
 labels = (labels[subset] .== " >50K") .* 2 .- 1 # converting to -1, 1
 
@@ -82,47 +95,39 @@ naive_preds = predict(forest_model, X_test)
 naive_acc = sum(naive_preds .== y_test) / size(X_test, 1)
 
 println("(Naive, Adaboost) accuracy: ($naive_acc, $adaboost_acc)")
-# (Naive, Adaboost) accuracy: (0.8225, 0.8365)
+# (Naive, Adaboost) accuracy: (0.801, 0.8185)
 
-using Plots
-
+# make visualizations
 n_estimators = [1, 10, 20, 50]
 
-plot(xlabel="n_estimators", ylabel="error", title="Train and Validation errors")
-for (name, idx) in (("train", 1), ("val", 2))
-    plot!(
-        n_estimators,
-        [m[idx] for m in metrics],
-        label="$name error",
-    )
-end
-plot!()
+p = plot(n_estimators, train_errs, label = "train_error")
+plot!(p, n_estimators, val_errs, label = "val_error")
+xlabel!(p, "Estimators")
+ylabel!(p, "Error")
+savefig(p, "./imgs/estimators_error.png")
 
-for (i, m) in zip(n_estimators, metrics)
-    histogram(m[3], label="weights", title="Weights distribution for $i estimators")
-    savefig("$(i)_distribution.png")
-end
+# as expected, the non-weighted train errors grows (new estimators have to 
+# learn less and less), but the validation error keeps lowering (as new 
+# learners' weights are smaller)
 
-# # FIXME I adapted a SVM implementation with JuMP to take in weights,
-# # but it is very slow and lacks regularization
-# function svm((X, y); weights=nothing)
-#     # from https://github.com/matbesancon/SimpleSVMs.jl/blob/master/src/SimpleSVMs.jl
+p = plot(
+    histogram.(weights_dist)..., 
+    layout = (4, 1), 
+    legend = false, 
+    sharex = true
+)
+p[:plot_title] = "Train sample weights"
+plot(p)
+savefig(p, "./imgs/train_sample_weights.png")
 
-#     n, p = size(X)
+# as expected, the train weights start uniform, and then gets concentrated
+# towards zero as most train observations have been learned by the
+# weak learners
 
-#     m = JuMP.Model(Ipopt.Optimizer)
-#     set_silent(m)
-#     @variable(m, W[1:p])
-#     @variable(m, b)
-#     @variable(m, l[1:n] >= 0)
-#     @constraint(m, l .>= 1 .- y .* (X * W .+ b))
-#     weights = weights === nothing ? ones(n) : weights
-#     @objective(m, Min, weights' * l)
-#     optimize!(m)
+p = histogram(estimators_weights, xlabel = "learner weight", ylabel = "frequence", label = "learner weight")
+savefig(p, "./imgs/learner_weights")
 
-#     svm_predict(X) = sign.(X * value.(W) .+ value.(b))
-#     return svm_predict
-# end
-# svm_model = svm((X_train, y_train))
-# y_pred = svm_model(X_test)
-# naive_acc = sum(y_pred .== y_test) / size(X_test, 1)
+# sum(estimators_weights .>= .2) # Out: 4
+# finally, it is also interesting to see that most of the weights for the
+# estimators are close to zero, with only 4 greater than `.2`, as they 
+# become responsible for learning less and less
